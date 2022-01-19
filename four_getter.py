be7f0d5d593855878b0f3bc7e9361a5485d7f34c
the-slash-bringing-sash-wringing <73411888+the-slash-bringing-sash-wringing@users.noreply.github.com>
2022-01-19 11:36:15 -0600

    Update four_getter.py

--- four_getter.py
+++ four_getter.py
@@ -1,5 +1,9 @@
 import basc_py4chan as chan
+from bs4 import BeautifulSoup
 import re
+
+def cleanup(string):
+    return BeautifulSoup(string, "lxml").text
 
 def get_finder(url):
     board=chan.board(url.split('/')[3])
@@ -7,7 +11,10 @@ def get_finder(url):
     posts= thread.all_posts
     output = []
     for x in posts:
-        if str(x.post_id)[-1:] == str(x.post_id)[-2:-1] or str(x.post_id)[-2:]==str(x.post_id)[-4:-2]:
-            if re.search('>>(.*)\n', x.text_comment) != None:
-                output.append([f'POST: {x.post_id} {x.text_comment}',[[f'REPLIED_TO: {post.post_id}', post.text_comment] for post in posts if post.post_id == int(re.search('>>(.*)\n', x.text_comment).group(1))]])
+        if str(x.post_id)[-1:] == str(x.post_id)[-2:-1] or str(x.post_id)[-2:]=='94' or str(x.post_id)[-2:]==str(x.post_id)[-4:-2]:
+            if re.search('href="#p(.*)"', x.comment) != None:
+                if re.search('>>\d+', cleanup(x.comment)) != None:
+                    rpi = re.match('>>\d+', cleanup(x.comment)).group().replace('>>', "")
+                    output.append({f'POST {x.post_id}' : f'{cleanup(x.comment)}', f'REPLIED TO {rpi}' : [f'{cleanup(post.comment)}' for post in posts \
+                                    if post.post_id == int(rpi)]})
     return output
