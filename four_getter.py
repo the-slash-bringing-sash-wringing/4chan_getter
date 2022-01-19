47606fa662f948f85af32f4613da28555a6f9b79
the-slash-bringing-sash-wringing <73411888+the-slash-bringing-sash-wringing@users.noreply.github.com>
2022-01-19 00:24:09 -0600

    Update four_getter.py

--- four_getter.py
+++ four_getter.py
@@ -7,7 +7,7 @@ def get_finder(url):
     posts= thread.all_posts
     output = []
     for x in posts:
-        if str(x.post_id)[-1:] == str(x.post_id)[-2:-1]:
+        if str(x.post_id)[-1:] == str(x.post_id)[-2:-1] or str(x.post_id)[-2:]==str(x.post_id)[-4:-2]:
             if re.search('>>(.*)\n', x.text_comment) != None:
                 output.append([f'POST: {x.post_id} {x.text_comment}',[[f'REPLIED_TO: {post.post_id}', post.text_comment] for post in posts if post.post_id == int(re.search('>>(.*)\n', x.text_comment).group(1))]])
     return output
